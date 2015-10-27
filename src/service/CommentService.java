882129da2b37ee7f35aed420544ecdc44a5f8acc
jin hubng wook <alechino@naver.com>
2015-10-27 17:55:17 +0900

    CommnetService 서비스 클래스 추가

---
+++ src/service/CommentService.java
@@ -0,0 +1,61 @@
+package service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import repository.CommentDao;
+import vo.Comment;
+
+@Component
+public class CommentService {
+	private CommentDao dao;
+
+	@Autowired
+	public void setDao(CommentDao dao) {
+		this.dao = dao;
+	}
+
+	public boolean writeComment(Comment comment) {
+		Date commentDate = new Date();
+		comment.setCommentDate(commentDate);
+		if (dao.commentWrite(comment) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	
+	public List<Comment> commentListView(int articleNo){
+		return dao.commentListView(articleNo);
+	}
+	
+	public boolean reWriteComment(int commentNo,String commentContent){
+		if(dao.commentReWrite(commentNo, commentContent)>0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean deleteComment(int commentNo){
+		if(dao.commentDelete(commentNo)>0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public int commentCount(int articleNo){
+		return dao.commnetCount(articleNo);
+	}
+	
+	public Comment readComment(int commentNo){
+		return dao.commentRead(commentNo);
+	}
+}
