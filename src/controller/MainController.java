801eabc3b469802a894d24505b3e61dde4a8dec9
jin hubng wook <alechino@naver.com>
2015-10-29 14:58:48 +0900

    controller의 Mapping되는 이름 변경 예:/c_write.do->c_write CommentController의 list 보기 기능을 메인 콘트롤러의 read로 옮김 faultylist도 read로 보낼 수 있게 함.

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,24 +1,44 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import service.CommentService;
+import service.FaultyService;
 import service.IndexService;
 import vo.Article;
 import vo.ArticlePage;
+import vo.Comment;
+import vo.Faulty;
 
 @Controller
 public class MainController {
 	private IndexService service;
+	private FaultyService fservice;
+	private CommentService cservice;
 	
 	@Autowired
 	public void setService(IndexService service) {
 		this.service = service;
 	}
 	
-	@RequestMapping("/index.do")
+	@Autowired
+	public void setCservice(CommentService cservice) {
+		this.cservice = cservice;
+	}
+	
+	@Autowired
+	public void setFservice(FaultyService fservice) {
+		this.fservice = fservice;
+	}
+	
+
+	
+	@RequestMapping("index")
 	public ModelAndView MainAllList(){
 		ArticlePage allNewPage=service.getMainArticlePage();
 		ArticlePage hotNewPage=service.getMainHotArticlePage();
@@ -31,12 +51,16 @@ public class MainController {
 		return mv;
 	}
 	
-	@RequestMapping("read.do")
+	@RequestMapping("read")
 	public ModelAndView selectView(int articleNo){
 		Article readArticle=service.readArticle(articleNo);
+		List<Comment> commentList=cservice.commentListView(articleNo);
+		List<Faulty> faultyList=fservice.listFaulty(articleNo);
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("read");
 		mv.addObject("readArticle", readArticle);
+		mv.addObject("commentList", commentList);
+		mv.addObject("faultyList",faultyList);
 		return mv;
 	}
 	
